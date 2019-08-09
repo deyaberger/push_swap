aa055d05a4f4fbde55831fa9565609fe70ca742b
Deya BERGER <dberger@e1r11p5.42.fr>
2019-08-09 16:30:30 +0200

    rotate a la norme

--- ft_rotate.c
+++ ft_rotate.c
@@ -6,20 +6,34 @@
 /*   By: dberger <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/08 17:59:03 by dberger           #+#    #+#             */
-/*   Updated: 2019/08/09 16:20:27 by dberger          ###   ########.fr       */
+/*   Updated: 2019/08/09 16:30:20 by dberger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void	ft_rot_down(t_stack *pile, t_elem *bottom, t_elem *top, t_elem *link)
+{
+	int		s;
+
+	s = 1;
+	pile->first = bottom;
+	bottom->next = top;
+	link = top;
+	while (s < pile->size - 1)
+	{
+		link = link->next;
+		s++;
+	}
+	link->next = NULL;
+}
 
 int		ft_rotate(t_stack *pile, int mode)
 {
 	t_elem	*top;
 	t_elem	*bottom;
 	t_elem	*link;
-	int		s;
 
-	s = 1;
 	if (!pile->first)
 		return (0);
 	top = pile->first;
@@ -34,16 +48,6 @@ int		ft_rotate(t_stack *pile, int mode)
 		top->next = NULL;
 	}
 	else if (mode == 2)
-	{
-		pile->first = bottom;
-		bottom->next = top;
-		link = top;
-		while (s < pile->size - 1)
-		{
-			link = link->next;
-			s++;
-		}
-		link->next = NULL;
-	}
+		ft_rot_down(pile, bottom, top, link);
 	return (1);
 }
