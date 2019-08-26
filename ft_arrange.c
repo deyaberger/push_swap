ffd9edbb7b4fdd63222e04a5484d90f0870ffc71
Deya BERGER <dberger@e1r10p5.42.fr>
2019-08-26 19:19:06 +0200

    algo de tri qui marche mais ca reste trres moche

--- ft_arrange.c
+++ ft_arrange.c
@@ -6,7 +6,7 @@
 /*   By: dberger <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/26 10:48:46 by dberger           #+#    #+#             */
-/*   Updated: 2019/08/26 17:45:15 by dberger          ###   ########.fr       */
+/*   Updated: 2019/08/26 19:18:32 by dberger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -112,7 +112,7 @@ void		ft_count_rr_rrr(t_instr *list)
 		else
 			list->rb = 0;
 	}
-	else if (total2 <= total1 && total2 <= total3)
+	else if (total2 < total1 && total2 <= total3)
 	{
 		if (list->ra <= list->rb)
 		{
@@ -132,7 +132,7 @@ void		ft_count_rr_rrr(t_instr *list)
 			list->rrb = 0;
 		}
 	}
-	else if (total3 <= total1 && total3 <= total2)
+	else if (total3 < total1 && total3 < total2)
 	{
 		if (list->rra <= list->rrb)
 		{
@@ -175,49 +175,54 @@ void		ft_count_instr(t_stack *a, t_stack *b, t_instr *list, t_elem *tmp)
 	ft_printf("\n AFTER CHANGE RR: \nra = %d, rra = %d, rb = %d, rrb = %d, rr = %d, rrr = %d, total list = %d\n", list->ra, list->rra, list->rb, list->rrb, list->rr, list->rrr, list->total);
 }
 
+void	ft_rotate_n(t_stack *pile, int mode, int x)
+{
+	while (x > 0)
+	{
+		ft_rotate(pile, mode);
+		x--;
+	}
+}
+
 void	ft_do_instruct(t_stack *a, t_stack *b, t_instr *good)
 {
 	int		i;
-	int		down;
+	int		mode;
 
-	down = 0;
 	i = 0;
-	if (good->rra >= good->ra)
-		i = good->ra;
-	else
+	mode = 0;
+	ft_printf("\n ***** \nGOOD: \nra = %d, rra = %d, rb = %d, rrb = %d, rr = %d, rrr = %d, total good = %d\n", good->ra, good->rra, good->rb, good->rrb, good->rr, good->rrr, good->total);
+	if (good->ra != 0)
 	{
-		i = good->rra;
-		down = 1;
+		ft_rotate_n(a, 1, good->ra);
+		write(0, "ra\n", 3);
 	}
-	ft_printf("\n ***** \nGOOD: \nra = %d, rra = %d, rb = %d, rrb = %d, total good = %d\n", good->ra, good->rra, good->rb, good->rrb, good->total);
-	if (i != 0)
+	if (good->rra != 0)
 	{
-		while (i > 0)
-		{
-			if (down == 0)
-				ft_rotate(a, 1);
-			else
-				ft_rotate(a, 2);
-			i--;
-		}
+		ft_rotate_n(a, 2, good->rra);
+		write(0, "rra\n", 4);
 	}
-	if (good->rrb > good->rb)
-		i = good->rb;
-	else
+	if (good->rb != 0)
 	{
-		i = good->rrb;
-		down = 1;
+		ft_rotate_n(b, 1, good->rb);
+		write(0, "rb\n", 4);
 	}
-	if (i != 0)
+	if (good->rrb != 0)
 	{
-		while (i > 0)
-		{
-			if (down == 0)
-				ft_rotate(b, 1);
-			else
-				ft_rotate(b, 2);
-			i--;
-		}
+		ft_rotate_n(b, 2, good->rrb);
+		write(0, "rrb\n", 4);
+	}
+	if (good->rr != 0)
+	{
+		ft_rotate_n(a, 1, good->rr);
+		ft_rotate_n(b, 1, good->rr);
+		write(0, "rr\n", 3);
+	}
+	if (good->rrr != 0)
+	{
+		ft_rotate_n(a, 2, good->rrr);
+		ft_rotate_n(b, 2, good->rrr);
+		write(0, "rrr\n", 4);
 	}
 }
 
@@ -243,19 +248,20 @@ int			ft_arrange(t_stack *a, t_stack *b, int argc)
 	while (a->size != 0)
 	{
 		tmp = a->first;
-		while (tmp->next)
+		while (tmp)
 		{
 			tmp->rank = i + 1;
 			if (tmp == a->first)
 			{
 				ft_count_instr(a, b, good, tmp);
-				ft_printf("tmp->nb1=%d\n", tmp->nb);
+				compare = good;
 			}
 			tmp = tmp->next;
-			tmp->rank = i + 2;
-			ft_printf("tmp->nb=%d\n", tmp->nb);
-			ft_count_instr(a, b, compare, tmp);
-		//	ft_printf("GOOD: \nra = %d, rra = %d, rb = %d, rrb = %d, total good = %d\nCOMPARE: \nra = %d, rra = %d, rb = %d, rrb = %d, total comp = %d\n", good->ra, good->rra, good->rb, good->rrb, good->total, compare->ra, compare->rra, compare->rb, compare->rrb, compare->total);
+			if (tmp)
+			{
+				tmp->rank = i + 2;
+				ft_count_instr(a, b, compare, tmp);
+			}
 			if (compare->total < good->total)
 				good = compare;
 			i++;
@@ -270,9 +276,5 @@ int			ft_arrange(t_stack *a, t_stack *b, int argc)
 		ft_printf("b = \n");
 		ft_list_print(b, 1);
 	}
-/*	ft_printf("a = \n");
-	ft_list_print(a, 1);
-	ft_printf("b = \n");
-	ft_list_print(b, 1);
-*/	return (1);
+	return (1);
 }
