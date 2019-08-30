d802d3f380991579ff812a90bfe32f7a7dac3393
Deya BERGER <dberger@e1r10p5.42.fr>
2019-08-30 12:09:33 +0200

    push swap a la norme

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: dberger <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/07 11:53:43 by dberger           #+#    #+#             */
-/*   Updated: 2019/08/29 12:44:21 by dberger          ###   ########.fr       */
+/*   Updated: 2019/08/30 12:09:10 by dberger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,9 @@ typedef struct		s_elem
 typedef struct		s_stack
 {
 	int				size;
+	int				op;
 	int				min;
-	int				max;
+	int				max1;
 	int				max2;
 	int				max3;
 	struct s_elem	*first;
@@ -50,15 +51,17 @@ typedef struct		s_instr
 
 int					ft_check_arg(char **argv);
 t_stack				*ft_create_stack(void);
-void				ft_print_instr(t_stack *a, t_stack *b, char *str, int color);
+void				ft_print_instr(t_stack *a, t_stack *b,
+					char *str, int color);
 void				ft_push(t_stack *pile, int n);
 void				ft_swap(t_stack *pile);
 int					ft_rotate(t_stack *pile, int mode);
 int					ft_del_elem(t_stack *pile);
 int					ft_instruct(t_stack *a, t_stack *b);
 int					ft_stack_order(t_stack *a, t_stack *b);
-void				ft_count_instr(t_stack *a, t_stack *b, t_instr *list, t_elem *tmp);
+void				ft_count_instr(t_stack *a, t_stack *b,
+					t_instr *list, t_elem *tmp);
 int					ft_arrange_b(t_stack *a, t_stack *b, int argc);
-void				ft_finish_swap(t_stack *a, t_stack *b);
+int					ft_finish_swap(t_stack *a, t_stack *b);
 
 #endif
